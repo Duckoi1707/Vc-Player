b51520a167d5b772ff536b76316f6a2bf7bb8266
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2021-12-27 19:16:56 +0700

    Add files via upload

--- EvaMaria/Just_fun_games.py
+++ EvaMaria/Just_fun_games.py
@@ -10,7 +10,19 @@ BOWLING = "🎳"
 TRY_YOUR_LUCK = "🎰"
 GOAL_E_MOJI = "⚽"
 BASKETBALL = "🏀"
+CHANLE = "🎲"
 
+@Client.on_message(filters.command(["chanle", "henxui"], prefixes=f"{HNDLR}"))
+async def chanle(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=CHANLE,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
 
 @Client.on_message(filters.command(["throw", "dart"], prefixes=f"{HNDLR}"))
 async def throw_dart(client, message):
