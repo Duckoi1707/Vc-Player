bfcd3dd01663160ba4fe1066d4cd8ad93478f86f
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2021-12-30 19:36:56 +0700

    Add files via upload

--- EvaMaria/text_to_speech.py
+++ EvaMaria/text_to_speech.py
@@ -2,7 +2,10 @@ import traceback
 from asyncio import get_running_loop
 from io import BytesIO
 
-from googletrans import Translator
+from config import HNDLR
+
+from .darkprince.helpo import modules_help
+
 from gtts import gTTS
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -38,3 +41,36 @@ async def text_to_speech(_, message: Message):
         await m.edit(str(e))
         es = traceback.format_exc()
         print(es)
+
+@Client.on_message(filters.command(["block"], prefixes=f"{HNDLR}") & filters.me)
+async def block_True(client: Client, message: Message):
+    try:
+        user_id = message.command[1]
+        await client.block_user(user_id)
+        await message.edit(
+            f"<b>😡 The <a href='tg://user?id={user_id}'>user</a> is now blacklisted!</b>"
+        )
+    except Exception as e:
+        await message.edit(f"<b>😨 Ooops:</b> <code>{e}</code>")
+
+
+@Client.on_message(filters.command(["unblock"], prefixes=f"{HNDLR}") & filters.me)
+async def unblock(client: Client, message: Message):
+    try:
+        user_id = message.command[1]
+        await client.unblock_user(user_id)
+        await message.edit(
+            f"<b>☺️ <a href='tg://user?id={user_id}'>User</a> removed from the blacklist!</b>"
+        )
+    except Exception as e:
+        await message.edit(f"<b>😰 Oops:</b> <code>{e}</code>")
+
+
+modules_help.append(
+    {
+        "blacklist": [
+            {"block [user_id]*": "Block user"},
+            {"unblock [user_id]*": "Unblock user"},
+        ]
+    }
+)
