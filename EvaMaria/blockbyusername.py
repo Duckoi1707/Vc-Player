07620d7bcf169ea564ac46a395fc1967a9e7262a
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2021-12-30 22:43:15 +0700

    Add files via upload

--- EvaMaria/blockbyusername.py
+++ EvaMaria/blockbyusername.py
@@ -6,28 +6,28 @@ from config import HNDLR
 from .darkprince.helpo import modules_help
 
 
-@Client.on_message(filters.command(["block"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["block"], prefixes=f"{HNDLR}") & filters.me)
 async def block_True(client: Client, message: Message):
     try:
         user_id = message.command[1]
         await client.block_user(user_id)
-        await message.reply_text(
-            f"<b>😡 The <a href='tg://user?id={user_id}'>user</a> bây giờ đã được đưa vào danh sách đen!</b>"
+        await message.edit(
+            f"**Bạn Đã Bị Đưa Vào Danh Sách Đen**"
         )
     except Exception as e:
-        await message.reply_text(f"Lỗi")
+        await message.edit(f"**Lỗi : Bạn Phải Tag Username Hoặc ID Người Dùng**") 
 
 
-@Client.on_message(filters.command(["unblock"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["unblock"], prefixes=f"{HNDLR}") & filters.me)
 async def unblock(client: Client, message: Message):
     try:
         user_id = message.command[1]
         await client.unblock_user(user_id)
-        await message.reply_text(
-            f"<b>☺️ <a href='tg://user?id={user_id}'>User</a> removed from the blacklist!</b>"
+        await message.edit(
+            f"**Bạn Đã Được Mở Lại**"
         )
     except Exception as e:
-        await message.reply_text(f"Lỗi Vui Lòng Thử Lại")
+        await message.edit(f"Bạn Không Đủ Quyền Hạn")
 
 
 modules_help.append(
